9a86bf9830561fcad6f6b261337ebd935f7bc788
zfliex <zfliex924@gmail.com>
2025-02-11 10:51:16 +0800

    f

--- generate-btclightclient.js
+++ generate-btclightclient.js
@@ -23,11 +23,12 @@ program.option("--rewardForValidatorSetChange <rewardForValidatorSetChange>",
 
 program.option("--initConsensusStateBytes <initConsensusStateBytes>",
     "init consensusState bytes, hex encoding, no prefix with 0x",
-    "00404b2878f7adc4720f60656d31fd5111fa1b526026bf1084d496c90300000000000000552135cb71df767cb6425e703e8223dce06e4f8b60fece9aef7ad54e5e2361fa37ba7d679a0a011953099de5");
+    "00805629400be5c2b190764772a2d65184b55687f8ef12c1ea5173230000000000000000833444ba5744737d9318db1cca7c49307a2f43a10048dbed4a31f472387eaee7300ea467c0ff3f1990e40d40");
     
 program.option("--initChainHeight <initChainHeight>",
     "init btc chain height",
-    3612672);
+    3771936
+);
 
 program.option("--mock <mock>",
     "if use mock",
