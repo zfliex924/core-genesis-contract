6bf1b5a8699986a870d6012a2ca0097e89b5f8cc
zfliex <zfliex924@gmail.com>
2025-10-09 10:33:11 +0800

    f

--- init_cycle.js
+++ init_cycle.js
@@ -1,7 +1,7 @@
 const web3 = require("web3")
 const init_cycle = {
     blockPeriod: 3,
-    epochLength: 200,
+    epochLength: 50,
     roundInterval: 1800,
     validatorCount: 7,
     turnLength: 1
